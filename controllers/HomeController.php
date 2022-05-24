25c93101e4e382a41aab682bb4d3eacf1eb655ca
usagui22 <danita9516@gmail.com>
2022-05-24 14:54:54 -0400

    ultimo cambio antes de nueva rama

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,11 +2,23 @@
 
 namespace app\controllers;
 
-class HomeController extends \yii\web\Controller
+use app\models\Usuario;
+use Yii;
+use yii\rest\ActiveController;
+
+class HomeController extends ActiveController
 {
     public function actionIndex()
     {
         return $this->render('index');
     }
+
+    public function actionLogin(){
+        
+        $id=Yii::$app->usuario->isGuest->getmygid;
+        $usuario=Usuario::findOne($id);
+        
+    }
+    
 
 }
