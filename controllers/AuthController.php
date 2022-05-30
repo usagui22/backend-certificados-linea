ebd66bbe449151eefee45c7c8110c27eef96d724
yurguen007 <201708315@est.umss.edu>
2022-05-30 13:24:05 -0400

    Cambios login

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -29,11 +29,41 @@ class AuthController extends Controller{
         $this->enableCsrfValidation = false;
         return parent::beforeAction($action);
     }
+
+    public function actionLogin(){
+        $params = Yii::$app->request->getBodyParams();
+        $usuario = Usuario::find()
+        ->where(["correo" => $params["correo"]])
+        ->one();
+
+        if($usuario){
+            if(Yii::$app->getSecurity()->validatePassword($params['password'],$usuario->password_hash)){
+                $response = [
+                    "status" => true,
+                    "msg" => "Login existoso"
+                ];
+            }else{
+                Yii::$app->response->statusCode = 500;
+                $response = [
+                    "status" => false,
+                    "msg"=> "Verifique sus datos"
+                ];
+            }
+        }else{
+            Yii::$app->response->statusCode = 500;
+            $response = [
+                "status" => false,
+                "msg"=> "Verifique sus datos"
+            ];
+        }
+        return $response;
+    }
     public function actionRegister(){
         $res = null;
         $params = Yii::$app->getRequest()->getBodyParams();
+        
         $usuario = Usuario::find()
-            ->where(["email" => $params["email"]])
+            ->where(["correo" => $params["correo"]])
             ->one();
         if($usuario){
             Yii::$app->response->statusCode = 200;
@@ -102,7 +132,6 @@ class AuthController extends Controller{
             "apellido_materno"=>$data['apellido_materno'],
             "correo"=>$data['correo'],
         ];
-
         $model = new Usuario($usuario);
         if($model->save()){
             if($this->asignarRol($model->id, "PTTE")){
@@ -130,6 +159,8 @@ class AuthController extends Controller{
         //     "imagen"=>$data['imagen'],
         //     "estado_civil"=>$data['estado_civil'],
         // ];
+        $contra = Yii::$app->getSecurity()->generatePasswordHash("123456789");
+        print_r($contra);
         $model = new Usuario($data);
         if($model->save()){
 
